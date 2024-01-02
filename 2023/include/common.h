b3b4e088ca972786ab20689d8d480f2540a2a1ce
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-01-02 14:03:16 -0600

    Day 1

--- 2023/include/common.h
+++ 2023/include/common.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <map>
 
 #include <fstream>
 #include <iostream>
--- 2023/src/day1.cpp
+++ 2023/src/day1.cpp
@@ -56,43 +56,87 @@ int partTwo () {
 
     int total = 0;
 
+    std::map<std::string, int> numAsWord;
+
+    numAsWord["one"] = 1;
+    numAsWord["two"] = 2;
+    numAsWord["three"] = 3;
+    numAsWord["four"] = 4;
+    numAsWord["five"] = 5;
+    numAsWord["six"] = 6;
+    numAsWord["seven"] = 7;
+    numAsWord["eight"] = 8;
+    numAsWord["nine"] = 9;
+    numAsWord["zero"] = 0;
+
     for (const auto& line : lines) {
         
         // two pointers
-        int i = 0, j = line.length() - 1;
+        int size = line.length();
+        int i = 0, j = size - 1;
 
         int firstCharacter = -1, lastCharacter = -1;
 
-        while (i <= j) {
+        while (i < size && j >= 0) {
             char iPointer = line.at(i);
             char jPointer = line.at(j);
 
-            if (std::isdigit(iPointer) && firstCharacter == -1) {
-                firstCharacter = digitToInt(iPointer);
-            }
-            else {
-                if (firstCharacter == -1) {
+            if (firstCharacter == -1) {
+                if (std::isdigit(iPointer)) {
+                    firstCharacter = digitToInt(iPointer);
+                }
+                else {
                     // check for substrings
+
+                    for ( const auto &[key, value]: numAsWord ) {
+                        int wordLen = i + key.length();
+                        if (wordLen < size) {
+                            if (line.substr(i, key.length()) == key) {
+                                firstCharacter = value;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (firstCharacter == -1) {
+                    i++;
                 }
             }
             
-            if (firstCharacter == -1) {
-                i++;
+            
+
+            if (lastCharacter == -1) {
+                if (std::isdigit(jPointer)) {
+                    lastCharacter = digitToInt(jPointer);
+                }
+                else {
+                    // check for substrings
+
+                    for ( const auto &[key, value]: numAsWord ) {
+                        if (j + key.length() <= size) {
+                            if (line.substr(j, key.length()) == key) {
+                                //std::cout << key << std::endl;
+                                lastCharacter = value;
+                                break;
+                            }
+                        }
+                    }
+                }
+                
+                if (lastCharacter == -1) {
+                    j--;
+                }
             }
 
-            if (std::isdigit(jPointer) && lastCharacter == -1) {
-                lastCharacter = digitToInt(jPointer);
-            }
-            else if (lastCharacter == -1) {
-                j--;
-            }
+            
 
             if (firstCharacter != -1 && lastCharacter != -1) {
                 break;
             }
         }
 
-        total += combineTwoNumbers(firstCharacter, lastCharacter);
+        int b =  combineTwoNumbers(firstCharacter, lastCharacter);
+        total += b;
     }
     
     return total;
@@ -100,6 +144,6 @@ int partTwo () {
 
 int main ()
 {
-     std::cout << "Part 1: " + std::to_string(partOne()) << std::endl;
+    std::cout << "Part 1: " + std::to_string(partOne()) << std::endl;
     std::cout << "Part 2: " + std::to_string(partTwo()) << std::endl;
 }
